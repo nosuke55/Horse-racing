543f2dcb7337c55375ca4be6e02e1213408397c9
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2019-12-24 15:07:06 +0900

    keiba,py fix retrun

--- src/keiba.py
+++ src/keiba.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from LightGBM import LightGBM#自作classのimport
-import lightgbm as lgb
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 # 学習する
 def fit(X_train, y_train, X_test, y_test):
+    """
+    2019/12/21
+    ・学習する関数。
+    ・modelはLogisticRegressionを使用。今のところLinearSVC, RandomForestClassifierより精度良かった。
+    ・まぁパラメーター何もいじってないんですけどね...
+    ・学習後に正答率と混合行列を表示する。
+    """
     # Create linear regression object
     regr = LogisticRegression(solver='lbfgs', max_iter=1000)
     # Train the model using the training set
@@ -18,15 +22,25 @@ def fit(X_train, y_train, X_test, y_test):
     predicted = regr.predict(X_test)
     # Check accuracy
     print(accuracy_score(y_test, predicted))
+    #print(predicted)
     mat = confusion_matrix(y_test, regr.predict(X_test))
     print(mat)
+    return predicted
 
 # 前処理
 def preprocessing(keibaData):
-    dropdData = keibaData.drop(['性齢', '騎手', '調教師', '風向', '日時'], axis=1)
-    newColumns = ["label", "frame", "horse_num", "horse_name", "burden_weight", "estimated_rise", "horse_weight", "fluctuation", "winning_popularity", 
-        "precipitation_amount", "temperature", "wind_speed", "rase", "distance", "frame2", "horse_num2", "horse_name2", "burden_weight2", "estimated_rise2", 
-        "horse_weight2", "fluctuation2", "winning_popularity2", "precipitation2", "temperature2", "wind_speed2", "rase2", "deistance2"]
+    """
+    2019/12/21
+    ・現時点では馬名以外の文字が含まれる列はすべて除去している。
+    ・除去しない場合は、dropをさせないのとnewColumnsにカラム名を追加する必要あり。
+    ・この前処理では、csvファイルを全て入れる必要がある。
+    例
+    ・1つのcsvのデータを11日だけを前処理, 12日だけを前処理などはできない。
+    """
+    dropdData = keibaData #.drop(['性齢', '騎手', '調教師', '風向', '日時'], axis=1)
+    newColumns = ["勝負", "枠", "馬番", "馬名", "性齢", "負担重量", "騎手", "推定上り", "馬体重", "増減", "調教師", "単勝人気", 
+        "降水量", "気温", "風速", "風向", "レース", "距離", "日時", "枠2", "馬番2", "馬名2", "性齢2", "負担重量2", "騎手2", "推定上り2", 
+        "馬体重2", "増減2", "調教師2", "単勝人気2", "降水量2", "気温2", "風速2", "風向2", "レース2", "距離2", "日時2"]
     merge = []
     delete = 0 # 次に削除する最初の番地の保持。
     while len(dropdData) != 0:
@@ -58,35 +72,51 @@ def preprocessing(keibaData):
                 gyou = np.concatenate([usiUma, maeUma])
                 gyou = np.insert(gyou, 0, wl)
                 merge.append(list(gyou))
-    newData = pd.DataFrame(merge, index=None, columns=newColumns,dtype='float64')
+    newData = pd.DataFrame(merge, index=None, columns=newColumns)
     #newData.to_csv("test.csv", encoding="utf-8-sig") # csvで書き出し
     return newData
 
+# 馬の順位を表示する
+def ranking(keibaTest, predicted):
+    """
+    馬の順位を表示する。
+    馬ごとにカウントする。そのため一番カウントが多い馬が1位と予想される。
+    """
+    rank = pd.DataFrame()
+    # predicted で勝ちと予想された行のみを取得
+    for p in range(len(predicted)):
+        if predicted[p] == '1':
+            rank = rank.append(keibaTest.iloc[p])
+    # 勝率順 - 馬名を馬ごとにカウントする
+    print(rank['馬名'].value_counts())
+
+
 if __name__ == "__main__":
-    lgbm=LightGBM()
-    keibaData = pd.read_csv("../data/201911.csv",sep=",")
-    keibaData = preprocessing(keibaData)
-    #keibaData.columns = ['a', 'b', 'c']
-    X = keibaData.drop(columns=['label', 'horse_name', 'horse_name2'])#label,horse_name,horse_name2
-    y = keibaData['label']
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=0)
-    print(X)
-    #fit(X_train, y_train, X_test, y_test)
-    hp=lgbm.hyperparm()
-    train_data=lgbm.train_data(X_train,y_train,lgb)
-    test_data=lgbm.test_data(X_test,y_test,train_data,lgb)
-    model=lgbm.fit(hp,train_data,test_data,lgb)
-    y_pred=lgbm.predict(X_test,model)
-    lgbm.accuracy_rate(y_test,y_pred)
-    #print("正解率",accuracy_score(y_test, y_pred))
-    #mat = confusion_matrix(y_test, y_pred)
-    y_pred_list = []
-    for x in y_pred:
-        y_pred_list.append(np.argmax(x))
-    mat = confusion_matrix(y_test, y_pred_list)
-    print(mat)
-    #print(y_test)
-    print('Accuracy score = \t {}'.format(accuracy_score(y_test, y_pred_list)))
-    print('Precision score = \t {}'.format(precision_score(y_test, y_pred_list)))
-    print('Recall score =   \t {}'.format(recall_score(y_test, y_pred_list)))
-    print('F1 score =      \t {}'.format(f1_score(y_test, y_pred_list)))
+    # csvを読み込む
+    keibaTrain = pd.read_csv("../data/201910-11.csv",sep=",")
+    keibaTest = pd.read_csv("../data/test.csv",sep=",")
+
+    # データの前処理
+    keibaTrain = preprocessing(keibaTrain)
+    # One-hot 日本語のカラムのみ処理する
+    keibaTrain = pd.get_dummies(keibaTrain, columns=['馬名', '性齢', '騎手', '調教師', '風向', '日時', '馬名2', '性齢2', '騎手2', '調教師2', '風向2', '日時2'])
+
+    keibaTest = preprocessing(keibaTest)
+    keibaTest2 = keibaTest.copy() # rankingの表示用
+    # One-hot 日本語のカラムのみ処理する
+    keibaTest = pd.get_dummies(keibaTest, columns=['馬名', '性齢', '騎手', '調教師', '風向', '日時', '馬名2', '性齢2', '騎手2', '調教師2', '風向2', '日時2'])
+    keibaTest = keibaTest.reindex(labels=keibaTrain.columns,fill_value=0, axis=1)
+
+    # データの準備
+    X_train = keibaTrain.drop(columns="勝負")
+    #X_train = keibaTrain.drop(columns=['勝負', '馬名', '性齢', '騎手', '調教師', '風向', '日時', '馬名2', '性齢2', '騎手2', '調教師2', '風向2', '日時2'])
+    y_train = keibaTrain["勝負"]
+
+    X_test = keibaTest.drop(columns="勝負")
+    #X_test = keibaTest.drop(columns=['勝負', '馬名', '性齢', '騎手', '調教師', '風向', '日時', '馬名2', '性齢2', '騎手2', '調教師2', '風向2', '日時2'])
+    y_test = keibaTest["勝負"]
+
+    # 学習する
+    predicted = fit(X_train, y_train, X_test, y_test)
+    # 順位を表示する
+    ranking(keibaTest2, predicted)
